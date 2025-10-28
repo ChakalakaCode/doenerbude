546c4596ddb0255f5e7f893a8705fc2e17efee7b
chakalakacode <mutlu.arabul.97@gmail.com>
2025-10-28 04:30:21 +0300

    yes

--- fixnav.js
+++ fixnav.js
@@ -40,17 +40,33 @@ function fixNavigation() {
                 const filePath = path.join(locationDir, file);
                 let content = fs.readFileSync(filePath, 'utf8');
                 
-                // Korrigiere den Standorte-Link in der Navigation
-                content = content.replace(
-                    /<a\s+href=["']#?standorte\.html["']/g,
-                    '<a href="/standorte.html"'
-                );
+                // Ersetze die komplette Navigation
+                const newNav = `
+                <nav class="navbar">
+                    <div class="nav-container">
+                        <div class="logo">
+                            <div class="logo-circle-new">
+                                <a href="/"><img src="../berlin.jpg" alt="Dönerbude Logo" class="logo-image"></a>
+                            </div>
+                            <a href="/" style="text-decoration: none;">
+                                <span class="logo-beelen">Beelen</span>
+                            </a>
+                        </div>
+                        <ul class="nav-menu">
+                            <li><a href="/#home" class="nav-link">Home</a></li>
+                            <li><a href="/#menu" class="nav-link">Speisekarte</a></li>
+                            <li><a href="/standorte.html" class="nav-link">Standorte</a></li>
+                            <li><a href="/#about" class="nav-link">Über uns</a></li>
+                            <li><a href="/#contact" class="nav-link">Kontakt</a></li>
+                            <li><a href="tel:+4925868828866" class="nav-link order-btn">
+                                <i class="fas fa-phone-alt"></i> 02586 88 2 88 66
+                            </a></li>
+                        </ul>
+                    </div>
+                </nav>`;
                 
-                // Füge die aktive Klasse zum Standorte-Link hinzu
-                content = content.replace(
-                    /<a\s+href=["']\/standorte\.html["'](?!.*active)/,
-                    '<a href="/standorte.html" class="nav-link active"'
-                );
+                // Ersetze die bestehende Navigation
+                content = content.replace(/<nav class="navbar">[\s\S]*?<\/nav>/, newNav);
                 
                 fs.writeFileSync(filePath, content);
                 console.log(`Navigation in ${file} aktualisiert`);
